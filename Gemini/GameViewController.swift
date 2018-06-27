9f5448754da53611a49ede8cecb85422dabb7451
Benjamin Yi <bentaebox@gmail.com>
2018-06-27 17:11:34 -0400

    add a pangesture on the left side of the board to start implementing tile selection

--- Gemini/GameViewController.swift
+++ Gemini/GameViewController.swift
@@ -9,28 +9,33 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    @IBOutlet weak var boardView: BoardView?
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let selectionArea = UIView.init(frame: CGRect.init(x: boardView!.frame.origin.x, y: boardView!.frame.origin.y, width: boardView!.frame.size.width / 2, height: boardView!.frame.size.height))
+        let selectionGesture = UIPanGestureRecognizer.init(target: self, action: #selector(selectTile))
+        
+        selectionArea.addGestureRecognizer(selectionGesture)
+        view.addSubview(selectionArea)
+    }
 
-        // Do any additional setup after loading the view.
+    @objc func selectTile(recognizer: UIPanGestureRecognizer) {
+        if recognizer.state == .began {
+            print("selection gesture STARTED")
+        }
+        
+        if recognizer.state == .ended {
+            print("selection gesture ENDED")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
